0aa363568bf7bf3ba476319edec8ab803be64d1c
Alaa Alshaer <asd2017591@gmail.com>
2021-11-22 16:10:57 +0200

    bottom navigation view with 4 fragment we use child fragment in Home navigation we save instance fragment

--- app/src/main/java/com/example/andriod_me/ui/HomeFragment.kt
+++ app/src/main/java/com/example/andriod_me/ui/HomeFragment.kt
@@ -8,7 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.GridView
-import androidx.activity.OnBackPressedCallback
+import androidx.activity.addCallback
 import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.commit
@@ -22,12 +22,8 @@ class HomeFragment(private val mBodyPartList: List<Int>) : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
 
-    private var mIndexHead = 0
-    private var mIndexBody = 0
-    private var mIndexLeg = 0
-
-    private var isNextButtonClicked = false
-    private var isSw600dpLayout = false
+    private var isBodyPartFragmentVisible = false
+    private var isTabletMode = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,177 +36,94 @@ class HomeFragment(private val mBodyPartList: List<Int>) : Fragment() {
             title = getString(R.string.title_home)
         }
 
-
-        val button = binding.fragmentMasterListBtnNext
-        val gridView = binding.fragmentMasterListGv
-
-        //may be null
-        val bodyPartFragmentContainerPhoneLayout = binding.fragmentHomeBodyPartContainer
-
-        binding.fragmentHomeLinearLayout?.let {
-            isSw600dpLayout = true
+        savedInstanceState?.let {
+            isBodyPartFragmentVisible = it.getBoolean(IS_BTN_CLICKED)
         }
 
-        activity?.onBackPressedDispatcher?.addCallback(
-            viewLifecycleOwner,
-            object : OnBackPressedCallback(true) {
-                override fun handleOnBackPressed() {
-                    if (!isSw600dpLayout) {
-                        isNextButtonClicked = false
-                        hideBodyPartFragmentContainerPhoneLayout(
-                            bodyPartFragmentContainerPhoneLayout!!,
-                            button!!,
-                            gridView
-                        )
-                        Log.d(TAG, "OnBackPressed")
-                    } else {
-                        isEnabled = false
-                        activity?.onBackPressed()
-                    }
-                }
-            })
+        val gridView = binding.fragmentMasterListGv
+        isTabletMode = binding.fragmentHomeSw600dpLinearLayout != null
 
         lateinit var bodyPartFragment: BodyPartFragment
 
-        binding.fragmentHomeLinearLayout?.let {
+        childFragmentManager.findFragmentByTag(BodyPartFragment.TAG)?.run {
+            bodyPartFragment = this as BodyPartFragment
+        } ?: run {
+            bodyPartFragment = BodyPartFragment()
+            bodyPartFragment.mBodyList = AndroidImageAsset.bodyList
+            bodyPartFragment.mHeadList = AndroidImageAsset.headList
+            bodyPartFragment.mLegList = AndroidImageAsset.legList
+        }
 
-            button?.visibility = View.GONE
+
+        if (isTabletMode) {
 
             val bodyPartFragmentContainerTabletLayout =
                 binding.fragmentHomeBodyPartFragmentContainer!!
 
+            savedInstanceState ?: addFragment(
+                bodyPartFragment,
+                bodyPartFragmentContainerTabletLayout.id,
+            )
 
+        } else {
 
+            val bodyPartFragmentContainerPhoneLayout =
+                binding.fragmentHomeBodyPartContainer!!
 
-            childFragmentManager.findFragmentByTag(BodyPartFragment.TAG)?.run {
-                bodyPartFragment = this as BodyPartFragment
-            } ?: run {
-                bodyPartFragment = BodyPartFragment()
-                bodyPartFragment.mBodyList = AndroidImageAsset.bodyList
-                bodyPartFragment.mHeadList = AndroidImageAsset.headList
-                bodyPartFragment.mLegList = AndroidImageAsset.legList
-            }
+            val button = binding.fragmentMasterListBtnNext!!
 
+            activity?.onBackPressedDispatcher
+                ?.addCallback(viewLifecycleOwner, true, {
+                    isBodyPartFragmentVisible = false
 
-            savedInstanceState?.apply {
-                Log.d(TAG, "savedInstanceState != NULL")
-
-            } ?: apply {
-                Log.d(TAG, "savedInstanceState == NULL")
-                childFragmentManager.commit {
-                    setReorderingAllowed(true)
-                    if (!bodyPartFragment.isAdded)
-                        add(
-                            bodyPartFragmentContainerTabletLayout.id,
-                            bodyPartFragment,
-                            BodyPartFragment.TAG
-                        )
-                }
-            }
-
-
-            gridView.setOnItemClickListener { _, _, position, _ ->
-                when (position / 12) {
-                    0 -> {
-                        bodyPartFragment.mHeadIds = selectImageIndex(position)
+                    childFragmentManager.commit {
+                        detach(bodyPartFragment)
                     }
-                    1 -> {
-                        bodyPartFragment.mBodyIds = selectImageIndex(position)
-                    }
+                    hideBodyPartFragmentContainerPhoneLayout(
+                        bodyPartFragmentContainerPhoneLayout,
+                        button,
+                        gridView
+                    )
+                })
 
-                    2 -> {
-                        bodyPartFragment.mLegIds = selectImageIndex(position)
-                    }
-                }
-                refreshFragment(bodyPartFragment)
-            }
-
-        } ?: let {
-
-            button!!
-            bodyPartFragmentContainerPhoneLayout!!
-
-            savedInstanceState?.let {
-                mIndexHead = it.getInt(BodyPartFragment.HEAD_INDEX)
-                mIndexBody = it.getInt(BodyPartFragment.BODY_INDEX)
-                mIndexLeg = it.getInt(BodyPartFragment.LEG_INDEX)
-                isNextButtonClicked = it.getBoolean(IS_BTN_CLICKED)
-
-            }
-
-
-            if (isNextButtonClicked)
+            if (isBodyPartFragmentVisible)
                 showBodyPartFragmentContainerPhoneLayout(
                     bodyPartFragmentContainerPhoneLayout,
                     button,
                     gridView
                 )
-
-            childFragmentManager.findFragmentByTag(BodyPartFragment.TAG)?.run {
-                bodyPartFragment = this as BodyPartFragment
-            } ?: run {
-                bodyPartFragment = BodyPartFragment()
-                bodyPartFragment.mBodyList = AndroidImageAsset.bodyList
-                bodyPartFragment.mHeadList = AndroidImageAsset.headList
-                bodyPartFragment.mLegList = AndroidImageAsset.legList
-            }
-
-
-
-            gridView.setOnItemClickListener { _, _, position, _ ->
-                when (position / 12) {
-                    0 -> {
-                        bodyPartFragment.mHeadIds = selectImageIndex(position)
-                    }
-                    1 -> {
-                        bodyPartFragment.mBodyIds = selectImageIndex(position)
-                    }
-
-                    2 -> {
-                        bodyPartFragment.mLegIds = selectImageIndex(position)
-                    }
-                }
-            }
 
             button.setOnClickListener {
-                isNextButtonClicked = true
+                isBodyPartFragmentVisible = true
                 showBodyPartFragmentContainerPhoneLayout(
                     bodyPartFragmentContainerPhoneLayout,
                     button,
                     gridView
                 )
 
-                if (bodyPartFragment.isAdded)
-                    refreshFragment(bodyPartFragment)
+                addFragment(
+                    bodyPartFragment,
+                    bodyPartFragmentContainerPhoneLayout.id,
+                )
 
-                childFragmentManager.commit {
-                    if (bodyPartFragment.isAdded)
-                        return@commit
-                    // Attach or add a fragment
-                    if (bodyPartFragment.isDetached) {
-                        attach(bodyPartFragment)
-                    } else {
-                        Log.d("FragmentManager", "add")
-                        add(
-                            bodyPartFragmentContainerPhoneLayout.id,
-                            bodyPartFragment,
-                            BodyPartFragment.TAG
-                        )
-                        setReorderingAllowed(true)
-                    }
-                    // Set the animation for this transaction
-                    setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                refreshFragment(bodyPartFragment)
+            }
+        }
 
-                }
+        gridView.setOnItemClickListener { _, _, position, _ ->
 
-
+            when (position / 12) {
+                0 -> bodyPartFragment.mHeadIds = selectImageIndex(position)
+                1 -> bodyPartFragment.mBodyIds = selectImageIndex(position)
+                2 -> bodyPartFragment.mLegIds = selectImageIndex(position)
             }
 
-
-            gridView.adapter = MasterListAdapter(requireContext(), mBodyPartList)
-
-
+            if (isTabletMode)
+                refreshFragment(bodyPartFragment)
         }
+
+        gridView.adapter = MasterListAdapter(requireContext(), mBodyPartList)
+
 
         return binding.root
     }
@@ -261,10 +174,24 @@ class HomeFragment(private val mBodyPartList: List<Int>) : Fragment() {
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putInt(BodyPartFragment.HEAD_INDEX, mIndexHead)
-        outState.putInt(BodyPartFragment.BODY_INDEX, mIndexBody)
-        outState.putInt(BodyPartFragment.LEG_INDEX, mIndexLeg)
-        outState.putBoolean(IS_BTN_CLICKED, isNextButtonClicked)
+        outState.putBoolean(IS_BTN_CLICKED, isBodyPartFragmentVisible)
+    }
+
+    private fun addFragment(fragment: BodyPartFragment, containerId: Int) {
+
+        childFragmentManager.commit {
+            setReorderingAllowed(true)
+            when {
+                fragment.isAdded -> return@commit
+                fragment.isDetached -> attach(fragment)
+                else -> add(
+                    containerId,
+                    fragment,
+                    BodyPartFragment.TAG
+                )
+            }
+            setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+        }
     }
 
 
